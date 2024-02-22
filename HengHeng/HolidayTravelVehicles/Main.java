398833f772407416304e796f0a02a4041ec0ae8a
Princhon Sriurairatana <142210004+Plaiprcsr@users.noreply.github.com>
2024-02-22 17:45:33 +0700

    Delete HengHeng/HolidayTravelVehicles/Main.java

--- HengHeng/HolidayTravelVehicles/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-
-
-    public static void main(String[] args){
-        Vehicle vehicle = new Vehicle("159758","Series 3","2022","BMW",3000000);
-        System.out.println(vehicle);
-    }
-}
